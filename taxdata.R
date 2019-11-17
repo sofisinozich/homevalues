9c637789efcb5ca4a280f9eb1d48ea12cf809741
Sofi Sinozich <sinozich@umd.edu>
2019-11-16 20:24:28 -0500

    Finalized tax data location stuff

--- taxdata.R
+++ taxdata.R
@@ -6,13 +6,14 @@ library(magrittr)
 
 
 # Pulling in the data -----------------------------------------------------
-library(RSocrata)
+# library(RSocrata)
 # proptax2018<- read.socrata(
 #   "https://data.montgomerycountymd.gov/resource/26vm-snmd.csv",
 #   app_token = Sys.getenv("socrata727token"),
 # )
 # names(proptax2018) <- gsub(" ","_",names(proptax2018))
 # proptax2018 %<>% as_tibble
+# This takes a while, so...
 # saveRDS(proptax2018,"data/moco_real_property_tax_2018.rds")
 proptax2018 <- readRDS("data/moco_real_property_tax_2018.rds")
 
@@ -45,49 +46,40 @@ missinggeo <- readRDS("geocoded_missings.rds")
 missinggeo %<>% rename(addlong = lon, addlat = lat, parcel_code=PARCEL_CODE)
 
 # Check which ones are still missing
-stillmissing <- propertytax %>% left_join(missinggeo, by = "parcel_code") %>% 
-  mutate(addlat = coalesce(addlat.x,addlat.y), addlong = coalesce(addlong.x,addlong.y), add = coalesce(add.x,add.y)) %>% 
-  select(-addlat.x,-addlat.y,-addlong.x,-addlong.y, -add.x, -add.y) %>% 
-  filter(is.na(addlat)) %>% 
-  select(parcel_code,location_1:addlong)
+# stillmissing <- propertytax %>% left_join(missinggeo, by = "parcel_code") %>% 
+#   mutate(addlat = coalesce(addlat.x,addlat.y), addlong = coalesce(addlong.x,addlong.y), add = coalesce(add.x,add.y)) %>% 
+#   select(-addlat.x,-addlat.y,-addlong.x,-addlong.y, -add.x, -add.y) %>% 
+#   filter(is.na(addlat)) %>% 
+#   select(parcel_code,location_1:addlong)
 
 # There are only 8 left, so just going to do these manually... faster than yelling at the Google API
 # write_csv(stillmissing, "data/manual_fill_missing.csv")
 # Don't do that again so you don't have to do this again!!
 
 stillmissing<-read_csv("data/manual_fill_missing.csv")
-# Note but ignore that there are two properties at the same address with different assessments
+# Note but ignore (for now) that there are two properties at the same address with different assessments
 
 # Add everything back to propertytax
-propertytax %>% coalesce_join(missinggeo,by="parcel_code") %>% coalesce_join(stillmissing,by="parcel_code") %>% 
-  filter(is.na(addlat))
-# 🙌 finally, everyone has a latlong location.
 propertytax %<>% coalesce_join(missinggeo,by="parcel_code") %>% coalesce_join(stillmissing,by="parcel_code")
+propertytax %>% filter(is.na(addlat))
+# 🙌 finally, everyone has a latlong location.
 
 # Double-check that everyone is in Montgomery County
 # Note that the lat-longs derived from the original dataset seem to point to the 
 # street immediately adjacent rather than the building location, so things will be fuzzed a little
-# But I think we can reasonably assume that the crs is 4326
-
-# Two stages: check to see if latlong falls within polygons and then check whether zip is a valid MoCo zip
-# Going to be conservative given the fuzziness around the latlongs and projection etc.
 
 library(sf)
 library(tigris)
-mocoshape_sf <- county_subdivisions(cb=TRUE,state="MD",county="Montgomery",year=2017,class="sf")
-st_crs(mocoshape_sf)
-# Want to convert this to API call but currently (11/16) hitting 500 error... why?
-# mocozips <- read.socrata(
-#   "https://data.montgomerycountymd.gov/resource/mmib-2cgz.csv",
-#   app_token = Sys.getenv("socrata727token"),
-# )
-# So using downloaded version instead
-validzips <- read_csv("data/Zip_Codes__Map_Service_.csv") %>% select(ZIPCODE) %>% unique %>% pull
-
+mocotracts <- tracts(state="MD",county="Montgomery",year=2018,class="sf") # Will use tract-level analysis later!
+st_crs(mocotracts)
+# All research leads to Google Maps latlong being 4326, so rolling with that
 propertytax %<>% st_as_sf(x =., coords = c("addlong", "addlat"),crs=4326) %>% st_transform(crs=4269) %>% 
-  mutate(inmoco = lengths(st_within(.,mocoshape_sf))) %>% 
-  filter(inmoco == 1 | (inmoco == 0 & zip_code %in% validzips)) # Keep anything where latlong is in MoCo OR ZIP is valid MoCo ZIP
+  st_join(.,mocotracts,join=st_within)
 # Note that this will take a while.⤴️
+
+propertytax %>% filter(is.na(NAME))
+# Removing ~200 seems reasonable - enough to get rid of the obvious ones but also not worth probing into much further.
+propertytax %<>% filter(!is.na(NAME))
 
 # Save into an RDS for convenience
 saveRDS(propertytax,"data/moco_real_property_tax_2018_cleaned.rds")
